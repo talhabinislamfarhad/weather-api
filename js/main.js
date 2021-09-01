999ccb6af9527e6d8cbdbe50e5f92eb783611aa3
talhabinislamfarhad <farhadislam02247@gmail.com>
2021-09-01 21:47:23 +0600

    2nd commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
 var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
-var temp = document.querySelector('.temp');
+var tempCel = document.querySelector('.temp-cel');
+var tempFar = document.querySelector('.temp-far');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button = document.querySelector('.submit');
 
 
 button.addEventListener('click', function (name) {
+  // if (input.value == '') {
+  //   alert('Please Enter a City Name');
+  // }
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=50a7aa80fa492fa92e874d23ad061374')
     .then(response => response.json())
     .then(data => {
-      var tempValue = data['main']['temp'];
+      var tempCelValue = data['main']['temp'];
+      var tempFarValue = data['main']['temp'];
       var nameValue = data['name'];
       var descValue = data['weather'][0]['description'];
 
-      main.innerHTML = nameValue;
-      desc.innerHTML = "Desc - " + descValue;
-      temp.innerHTML = "Temp - " + parseInt(tempValue - 273.15) + "°C";
+      main.innerHTML = "Weather Of " + nameValue + " city";
+      desc.innerHTML = "Description - " + descValue;
+      tempCel.innerHTML = "Temperature - " + parseInt(tempCelValue - 273.15) + "°C.";
+      tempFar.innerHTML = "Temperature - " + parseInt((tempFarValue - 273.15) * 9 / 5 + 32) + "°F.";
       input.value = "";
 
     })
